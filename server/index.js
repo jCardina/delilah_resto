b12ed02792487c75e9e83ef11941ad85712d3362
jCardina <60352221+jCardina@users.noreply.github.com>
2020-02-18 00:18:18 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,3 +8,102 @@ server.use(bodyParser.json());
 server.listen(3000, () => {
     console.log("Servidor iniciado...");
 });
+
+
+//------------base de datos provisoria
+
+let users = [
+    {
+        id: 1,
+        name: "Pablo Lopez",
+        userName: "pabloLop",
+        email: "pablolopez@gmail.com",
+        address: "Doblas 243",
+        phoneNumber: 49675588,
+        password: passW,
+        admin: true
+    },
+    {
+        id: 2,
+        name: "Maria Gonzalez",
+        userName: "marGon",
+        email: "mariagonzalez@gmail.com",
+        address: "Peru 5870",
+        phoneNumber: 45635518,
+        password: passWoo,
+        admin: false
+    },
+    {
+        id: 3,
+        name: "Marcos Perez",
+        userName: "marPer",
+        email: "marcosperez@gmail.com",
+        address: "Ayacucho 1240",
+        phoneNumber: 45031319,
+        password: passWoo234,
+        admin: false
+    }
+
+];
+
+let products = [
+    {  
+        id: 1,
+        name: "Focaccia",
+        keyword: "focacc",
+        price: 200,
+        photoUrl: "https://www.gimmesomeoven.com/wp-content/uploads/2017/03/Rosemary-Focaccia-Recipe-1.jpg"
+    }
+    {  
+        id: 2,
+        name: "Veggie Burger",
+        keyword: "veggBur",
+        price: 150,
+        photoUrl: "https://storage.googleapis.com/gen-atmedia/3/2019/05/a94cfde51967df5caf0f1641f53a5470df4421c1.jpeg"
+    }
+
+];
+
+let orders = [
+    {
+        orderId: 1,
+        products: [
+          {
+            id: 2,
+            quantity: 1
+          },
+          {
+            id: 1,
+            quantity: 2
+          },
+        ],
+        total: 550,
+        userId: 2,
+        paymentMethod: "efectivo",
+        timeStamp: "18:29",
+        date: "10/02/2020",
+        status: "confirmado"
+      },
+      {
+        orderId: 2,
+        products: [
+          {
+            id: 1,
+            quantity: 1
+          },
+          {
+            id: 2,
+            quantity: 1
+          },
+        ],
+        total: 350,
+        userId: 3,
+        paymentMethod: "tarjeta",
+        timeStamp: "17:50",
+        date: "10/02/2020",
+        status: "nuevo"
+      }
+];
+
+
+//---------------------------------
