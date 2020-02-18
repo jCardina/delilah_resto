7726bc9c30cb61afeacc4c4bbf1b51c03b8f14d6
jCardina <60352221+jCardina@users.noreply.github.com>
2020-02-18 01:17:50 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,108 @@ server.listen(3000, () => {
     console.log("Servidor iniciado...");
 });
 
+//agregar respuestas de errores y codigos exito/error y middlewares (token y same user)
+
+//---------------productos
+
+server.get('/products', (request, response) => {
+    response.json(products);
+});
+
+server.post('/products', (request, response) => {
+    let {name, keyword, price, photoUrl} = request.body;
+    
+    let lastAssignedId = products[products.length - 1].id;
+    let newID = lastAssignedId + 1;
+
+    let newProduct = {
+        id: newID,
+        name: name,
+        keyword: keyword,
+        price: price,
+        photoUrl: photoUrl
+    };
+
+    products.push(newProduct);
+    console.log(products);
+
+    response.statusCode = 201;
+    response.json(newProduct);
+});
+
+server.get('/products/:id', (request, response) => {
+    const id = request.params.id;
+    
+    products.forEach(element => {
+        if(element.id == id) {
+            response.json(element);
+        }
+    });
+    
+});
+
+server.put('/products/:id', (request, response) => { //o patch? // 204 para put?
+    const id = request.params.id;
+    let {name, keyword, price, photoUrl} = request.body;
+
+    products.forEach(element => {
+        if(element.id == id) {
+
+            element.name = name;
+            element.keyword = keyword;
+            element.price = price;
+            element.photoUrl = photoUrl;
+
+            response.json(element);
+            console.log(products);
+        }
+    });
+    
+});
+
+server.delete('/products/:id', (request, response) => {
+    const id = request.params.id;
+    
+    products.forEach(element => {
+        if(element.id == id) {
+
+            let index = products.indexOf(element);
+
+            products.splice(index,1);
+
+            console.log(products);
+
+            response.statusCode = 204;
+            response.send();
+        }
+    });
+    
+});
+
+
+//-------------usuarios
+
+server.get('/users', (request, response) => {
+    let usersPublic = [];
+
+    users.forEach (element => {
+        let publicData = {
+            id: element.id,
+            userName: element.userName,
+            admin: element.admin
+        }
+        usersPublic.push(publicData);
+    });
+    response.json(usersPublic);
+});
+
+
+//---pedidos
+
+server.get('/orders', (request, response) => {
+    response.json(orders);
+});
+
 
 //------------base de datos provisoria
 
@@ -20,7 +122,7 @@ let users = [
         email: "pablolopez@gmail.com",
         address: "Doblas 243",
         phoneNumber: 49675588,
-        password: passW,
+        password: "passW",
         admin: true
     },
     {
@@ -30,7 +132,7 @@ let users = [
         email: "mariagonzalez@gmail.com",
         address: "Peru 5870",
         phoneNumber: 45635518,
-        password: passWoo,
+        password: "passWoo",
         admin: false
     },
     {
@@ -40,7 +142,7 @@ let users = [
         email: "marcosperez@gmail.com",
         address: "Ayacucho 1240",
         phoneNumber: 45031319,
-        password: passWoo234,
+        password: "passWoo234",
         admin: false
     }
 
@@ -53,7 +155,7 @@ let products = [
         keyword: "focacc",
         price: 200,
         photoUrl: "https://www.gimmesomeoven.com/wp-content/uploads/2017/03/Rosemary-Focaccia-Recipe-1.jpg"
-    }
+    },
     {  
         id: 2,
         name: "Veggie Burger",
